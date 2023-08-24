51f452d801a784825b87ef58d6078f963a60fa01
oussama di <ousstheouss@outlook.com>
2023-08-24 14:29:36 +0100

    added the navbar and the form page

--- Models/models.py
+++ Models/models.py
@@ -8,7 +8,49 @@ class Employee(db.Model):
     telephone = db.Column(db.String, unique=False, nullable=False)
     requests = db.relationship('VacationRequest', backref='employee')
 
+    def __repr__(self):
+        return f'Employee {self.id}: {self.first_name} {self.last_name}'
+    
+    def toJSON(self):
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'telephone': self.telephone
+        }
+
+
 class VacationRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, unique=False, nullable=True)
     employee_id = db.Column(db.Ineteger, db.ForeignKey('employee.id', nullable=False))
+
+    def __repr__(self):
+        return f'Request {self.id} done by Employee {self.employee_id}'
+
+    def toJSON(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+        }
+    
+
+class HR(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), unique=False, nullable=False)
+    last_name = db.Column(db.String(20), unique=False, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    telephone = db.Column(db.String, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'HR Employee {self.id}: {self.first_name} {self.last_name}'
+    
+    def toJSON(self):
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'telephone': self.telephone
+        }
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,3 +8,13 @@ db = SQLAlchemy()
 def hello_world():
 	return "<p>Hello, world</p>"
 	
+@app.route('/home')
+def home():
+	return render_template('/user/profil.html')
+
+@app.route("/nouvelle_demande")
+def nouvelle_demande():
+	return render_template("/user/nouvelleDemande.html")
+
+if __name__ == '__main__':
+	app.run(debug=True)
