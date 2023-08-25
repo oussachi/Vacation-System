b5aa0cf651eaa747024cc376406e9a4d90df7eb2
oussama di <ousstheouss@outlook.com>
2023-08-25 14:58:57 +0100

    updated the Employee model

--- Models/models.py
+++ Models/models.py
@@ -2,10 +2,17 @@ from app import db
 
 class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(20), unique=False, nullable=False)
-    last_name = db.Column(db.String(20), unique=False, nullable=False)
+    prénom = db.Column(db.String(20), unique=False, nullable=False)
+    nom = db.Column(db.String(20), unique=False, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     telephone = db.Column(db.String, unique=False, nullable=False)
+    date_de_naissance = db.Column(db.String, unique=False, nullable=False)
+    lieu_de_naissance = db.Column(db.String, unique=False, nullable=False)
+    poste = db.Column(db.String, unique=False, nullable=False)
+    service = db.Column(db.String, unique=False, nullable=False)
+    profil = db.Column(db.String, unique=False, nullable=False)
+    congé_acquis_n = db.Column(db.Integer, unique=False, nullable=False)
+    congé_acquis_n_1 = db.Column(db.Integer, unique=False, nullable=False)
     requests = db.relationship('VacationRequest', backref='employee')
 
     def __repr__(self):
@@ -13,11 +20,17 @@ class Employee(db.Model):
     
     def toJSON(self):
         return {
-            'id': self.id,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email': self.email,
-            'telephone': self.telephone
+                "prénom" : self.prénom,
+                "nom" : self.nom,
+                "email" : self.email,
+                "telephone" : self.telephone,
+                "date_de_naissance" : self.date_de_naissance,
+                "lieu_de_naissance" : self.lieu_de_naissance,
+                "poste" : self.poste,
+                "service" : self.service,
+                "profil" : self.profil,
+                "congé_acquis_n" : self.congé_acquis_n,
+                "congé_acquis_n_1" : self.congé_acquis_n_1
         }
 
 
