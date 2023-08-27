17a71e68bf010ca05f192613b5e93254f9d61ddc
oussama di <ousstheouss@outlook.com>
2023-08-27 18:42:09 +0100

    added some GRH pages

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ from Controllers import xmlController
 app = Flask(__name__)
 db = SQLAlchemy()
 
+#Login endpoint
 @app.route("/")
 def hello_world():
 	return render_template('login.html')
-	
-@app.route('/home')
-def home():
+
+
+# ----------------------------------------- Employee Endpoints ------------------------------ #
+@app.route('/home_user')
+def home_user():
 	user = xmlController.getElementsByTag('./file.xml', 'user')[0]
-	print(user)
 	return render_template('/user/profil.html', user=user)
 
 @app.route("/nouvelle_demande")
@@ -32,6 +34,31 @@ def mes_demandes():
 @app.route("/demande")
 def demande():
 	return render_template("/user/demande.html")
+
+
+
+# ----------------------------------------- GRH Endpoints ------------------------------ #
+@app.route("/home_grh")
+def home_grh():
+	return render_template("/GRH/profil.html")
+
+@app.route("/liste_demandes")
+def liste_demande():
+	demandes = [{
+		"date_debut" : datetime.datetime.now(),
+		"date_fin" : datetime.datetime.now(),
+		"statut" : "Processing"
+	}]
+	return render_template("/GRH/listeDemandes.html", demandes=demandes)
+
+@app.route("/details_demande")
+def details_demande():
+	return render_template("/GRH/detailsDemande.html")
+
+
+# ----------------------------------------- Manager Endpoints ------------------------------ #
+# ----------------------------------------- Admin Endpoints ------------------------------ #
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
